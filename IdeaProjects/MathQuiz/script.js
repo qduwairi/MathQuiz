06e5fdea1028f7e008afe9b21ab35a1999e8d862
Qasim I. Duwairi <qduwairi@gmail.com>
2024-04-24 13:52:50 +0300

    counter feature added

--- IdeaProjects/MathQuiz/script.js
+++ IdeaProjects/MathQuiz/script.js
@@ -1,10 +1,32 @@
 let answer;
+let correctCount = 0;
+let incorrectCount = 0;
 
 function generateQuestion() {
     const number1 = Math.floor(Math.random() * 100 + 1);
     const number2 = Math.floor(Math.random() * 100 + 1);
-    answer = number1 - number2;
+
+    //switch numbers to keep the value of the subtraction positive
+    if(number2 > number1){
+        let temp = number1;
+        number1 = number2;
+        number2 = temp;
+    } else {
+        answer = number1 - number2;
+    }
+
+    //display the question
     document.getElementById('question').innerText = `${number1} - ${number2} = ?`;
+}
+
+function updateCounters(correct){
+    if(correct) {
+        correctCount++;
+        document.getElementById('correctCounter').innerText = correctCount;
+    } else {
+        incorrectCount++;
+        document.getElementById('incorrectCounter').innerText = incorrectCount;
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -15,8 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const userAnswer = parseInt(document.getElementById('answer').value, 10);
         if (userAnswer === answer) {
             document.getElementById('result').innerText = 'Correct';
+            updateCounters(true);
         } else {
             document.getElementById('result').innerText = 'Incorrect. Try again!';
+            updateCounters(false);
         }
 
         generateQuestion();
